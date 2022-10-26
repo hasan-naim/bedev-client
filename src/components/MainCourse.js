da7ab76c8564f69a116349537862056b326291d0
hasan-naim <hasannaim6246@gmail.com>
2022-10-26 22:09:49 +0600

    courses page data fetched and rendered in the ui

--- src/components/MainCourse.js
+++ src/components/MainCourse.js
@@ -1,92 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function MainCourse() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://web-dev-course-server.vercel.app/courses")
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="lg:col-span-4 grid grid-cols-1 lg:grid-cols-3 gap-6">
-      <div className="card card-compact  bg-base-200 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Details</button>
+      {data.map((obj) => {
+        return (
+          <div
+            key={obj.id}
+            className="card card-compact  bg-base-200 shadow-xl"
+          >
+            <figure>
+              <img src={obj.img} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{obj.name}!</h2>
+              <p>{obj.description}</p>
+              <div className="card-actions justify-end">
+                <Link to={`/courses/${obj.id}`} className="btn btn-primary">
+                  Details
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="card card-compact  bg-base-200 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Details</button>
-          </div>
-        </div>
-      </div>
-      <div className="card card-compact  bg-base-200 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Details</button>
-          </div>
-        </div>
-      </div>
-      <div className="card card-compact  bg-base-200 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Details</button>
-          </div>
-        </div>
-      </div>
-      <div className="card card-compact  bg-base-200 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Details</button>
-          </div>
-        </div>
-      </div>
-      <div className="card card-compact  bg-base-200 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Details</button>
-          </div>
-        </div>
-      </div>
-      <div className="card card-compact  bg-base-200 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Details</button>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Sidebar() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://web-dev-course-server.vercel.app/catagories")
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="lg:col-span-2 bg-base-200 rounded-lg p-6">
       <h3 className="text-4xl text-gray-300 font-semibold text-center mb-4">
         List of Course
       </h3>
       <ul className="menu  w-full p-2 rounded-box">
-        <li>
-          <Link>Item 1</Link>
-        </li>
-        <li>
-          <Link>Item 2</Link>
-        </li>
-        <li>
-          <Link>Item 3</Link>
-        </li>
+        {data.map((obj) => {
+          return (
+            <li key={obj.id}>
+              <Link to={`/courses/${obj.id}`}>{obj.name}</Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
