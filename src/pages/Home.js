530e7c99a804730ba093fe461333b520c94e3f3b
hasan-naim <hasannaim6246@gmail.com>
2022-10-27 11:54:26 +0600

    home page is now done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthProvider";
 
 function Home() {
-  return <div>hoem</div>;
+  return (
+    <div className="pt-16">
+      <div className="container text-center">
+        <h1 class="text-white font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+          Welcome to Tutorial Hub
+        </h1>
+        <p class="mt-6 text-lg text-slate-400 text-center max-w-3xl mx-auto dark:text-slate-400">
+          Here you will find tutorial about web development. You can learn
+          everything about web development.
+        </p>
+        <Link to={`/courses`} className="btn btn-primary mt-6">
+          Courses
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
